946192f435d1ff6f00158e8fc739b069a52dd53d
Amrudin Balić <amrudinbalic@192.168.1.6>
2026-06-27 13:26:32 +0200

    Key Entities Products/Brands/Categories

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -12,7 +13,13 @@ class ProductController extends Controller
      */
     public function index()
     {
-        
+        $perPage = 15;
+
+        $products = Product::where('is_active', true)
+            ->latest()
+            ->paginate($perPage);
+
+        return response()->json($products);
     }
 
     /**
